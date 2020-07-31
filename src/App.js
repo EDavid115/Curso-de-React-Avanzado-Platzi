42195a288b4b19af072255b41293697c9b719d68
Ernesto David Cabana Pabón <ernesto@ideabti.com>
2020-07-30 20:28:03 -0500

    Instalando Style-Components, usando estilos globales y creando componentes de ListOfCategories y Category

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GlobalStyle } from './GlobalStyles'
+import { ListOfCategories } from './components/ListOfCategories'
+
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
+)
