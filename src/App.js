9c2f302482499d93e394b8d9cd5febd45c3f656d
Ernesto David Cabana Pabón <ernesto@ideabti.com>
2020-07-30 21:06:39 -0500

    Se añade react-icons, Se crea componente ListOfPhotoCards y PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
   <div>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 )
