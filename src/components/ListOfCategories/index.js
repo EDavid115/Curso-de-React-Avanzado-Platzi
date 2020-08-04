da10be4ac076220cd41b4b0f05de2ca1e7d7f4cb
Ernesto David Cabana Pabón <ernesto@ideabti.com>
2020-08-03 20:26:30 -0500

    Plugin-transform-runtime para soportart async/await, window.fetch para traer categorias de la api e implementar primeros useState y useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { Item, List } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    try {
+      const fetchCategories = async () => {
+        const res = await window.fetch('https://petgram-server-edcp.vercel.app/categories')
+        const data = await res.json()
+        setCategories(data)
+      }
+      fetchCategories()
+    } catch (err) {
+      console.log('fetchCategories Error: ', err)
+      setCategories([])
+    }
+  }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll())
+
+    return () => {
+      document.removeEventListener('scroll', onScroll())
+    }
+  })
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
