82b66b1eeffce4bdbbd200875fef02dda341cf10
ErnestoCDesktop <ernestocabanap@gmail.com>
2020-08-14 07:35:39 -0500

    Estilos y personalización por props del form

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -6,7 +6,10 @@ export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
       ({ activateAuth }) => {
-        return <UserForm onSubmit={activateAuth} />
+        return <>
+          <UserForm title='Registrarse' onSubmit={activateAuth} />
+          <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+        </>
       }
     }
   </Context.Consumer>
