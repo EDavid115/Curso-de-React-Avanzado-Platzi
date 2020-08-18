2ee0129173b7737aea68eeb5811c0b9c8eec683b
ErnestoCDesktop <ernestocabanap@gmail.com>
2020-08-18 12:43:42 -0500

    UseLoginMutation

---
+++ src/hooks-apollo/useLoginMutation.js
@@ -0,0 +1,14 @@
+import { useMutation } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+const LOGIN = gql`
+	mutation login ($input: UserCredentials!) {
+		login (input: $input)
+	}
+`
+
+export const useLoginMutation = () => {
+  const [loginMutation, { data, loading, error }] = useMutation(LOGIN)
+
+  return { loginMutation, dataLogin: data, loadingLogin: loading, errorLogin: error }
+}
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,18 +2,24 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { useRegisterMutation } from '../hooks-apollo/useRegisterMutation'
+import { useLoginMutation } from '../hooks-apollo/useLoginMutation'
 
 export const NotRegisteredUser = () => {
   const { registerMutation, loading, error } = useRegisterMutation()
+  const { loginMutation, loadingLogin, errorLogin } = useLoginMutation()
+
   const registerTitle = `Registra${loading ? 'ndose' : 'rse'}`
   const errorMsg = error && 'El usuario ya existe o hay algún problema.'
+
+  const loginTitle = `Inicia${loadingLogin ? 'ndo sesión' : 'r sesión'}`
+  const errorMsgLogin = errorLogin && 'Usuario o contraseña incorrecta.'
 
   return (
     <Context.Consumer>
       {
         ({ activateAuth }) => {
 
-          const onSubmit = ({ email, password }) => {
+          const onSubmitRegister = ({ email, password }) => {
             const input = {
               email: email.value,
               password: password.value
@@ -28,12 +34,28 @@ export const NotRegisteredUser = () => {
               .catch((err) => {
                 console.log('err', err)
               })
-            // activateAuth()
+          }
+
+          const onSubmitLogin = ({ email, password }) => {
+            const input = {
+              email: email.value,
+              password: password.value
+            }
+            const variables = { input }
+
+            loginMutation({ variables })
+              .then((res) => {
+                console.log('res', res)
+                activateAuth()
+              })
+              .catch((err) => {
+                console.log('err', err)
+              })
           }
 
           return <>
-            <UserForm title={registerTitle} onSubmit={onSubmit} error={errorMsg} disabled={loading} />
-            <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+            <UserForm title={registerTitle} onSubmit={onSubmitRegister} error={errorMsg} disabled={loading} />
+            <UserForm title={loginTitle} onSubmit={onSubmitLogin} error={errorMsgLogin} disabled={loadingLogin} />
           </>
         }
       }
