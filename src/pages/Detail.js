366fa563bdc84feaddc68fde3cb32c1b00fb2ed4
ErnestoCP <ernesto@ideabti.com>
2020-09-27 17:12:57 -0500

    Implementación de React-Helmet

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { PhotoCardWithQuery } from '../components/PhotoCardWithQuery'
+import { Layout } from '../components/Layout'
 
 export const Detail = ({ detailId }) => (
-  <PhotoCardWithQuery id={detailId} />
+  <Layout title={`Fotografía ${detailId}`}>
+    <PhotoCardWithQuery id={detailId} />
+  </Layout>
 )
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useGetFavorites } from '../hooks-apollo/useGetFavs'
 import { ListOfFavs } from '../components/ListOfFavs'
+import { Layout } from '../components/Layout'
 
 export const Favs = () => {
   const { data, loading, error } = useGetFavorites()
@@ -8,5 +9,9 @@ export const Favs = () => {
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error!</p>
 
-  return <ListOfFavs favs={data.favs} />
+  return (
+    <Layout title='Tus favoritos' subtitle='Aquí salen tus favoritos'>
+      <ListOfFavs favs={data.favs} />
+    </Layout>
+  )
 }
