d73f5fb2d656c8054f8e13de38f7461c62454cef
ErnestoCP <ernesto@ideabti.com>
2020-09-27 13:53:51 -0500

    Page Favorites, Cerrar sesión, SubmitButton

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { useGetFavorites } from '../hooks-apollo/useGetFavs'
+import { ListOfFavs } from '../components/ListOfFavs'
 
-export const Favs = () => (
-  <h1>Favs</h1>
-)
+export const Favs = () => {
+  const { data, loading, error } = useGetFavorites()
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
+
+  return <ListOfFavs favs={data.favs} />
+}
